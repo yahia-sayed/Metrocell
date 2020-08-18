62e7c488e777cd3a8451b91ec27534e9ff79d579
Yahia Sayed <48311058+YahiaSayed99@users.noreply.github.com>
2020-08-19 00:11:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <?php
-      <p align="center"><img src="Documents/logo.ico" alt=""/></p>
+      <h1 align="center"><img src="Documents/logo.ico" alt=""/></h1>
       <h1 align="center"><font color="66ffff">Yahia Sayed</h1>
-      <h1 align="center"><font color="66ffff"><a 	href="https://drive.google.com/drive/folders/1FH2LFkRd1FMV1UqMPqjdyR5wUZfPUoOV" 	target="_blank">My Projects</p>
-    
+      <h1 align="center"><font color="66ffff"><a 	href="https://drive.google.com/drive/folders/1FH2LFkRd1FMV1UqMPqjdyR5wUZfPUoOV" 	target="_blank">My Projects</h1>
 ?>
